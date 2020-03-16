5e8f8c4d24ad95b19387a44364a4883dc2bf8a39
Balázs Izsóf <balazs.izsof@gmail.com>
2020-03-16 19:24:42 +0100

    refactor init: main idea that parsing the urls of the search from the first page and not just the page number, so creating url step can be skipped

--- hasznaltauto_lib.py
+++ hasznaltauto_lib.py
@@ -2,29 +2,27 @@ import urllib.request
 import urllib.parse
 import re
 
-def __parse_url_html(url):
-    f = urllib.request.urlopen(url)
-    return f.read().decode('utf-8')
-
 def parse_cars(url):
-    cars = []
-    last_page_number = __parse_last_page_number(__parse_url_html(url))
+    first_page_html = get_html(url)
+    urls = [first_page_html, __parse_all_pages_url(first_page_html)]
 
-    for i in range(1,last_page_number):
-        current_page_html = __parse_page_html(url, i)
-        current_cars = __parse_page_cars(current_page_html)
+    cars = []
+    for url in urls:
+        current_cars = __parse_page_cars(url)
         cars.extend(current_cars)
 
     return cars
 
-def __parse_last_page_number(html):
-    page_number = re.search("(last.*?)(https.*?page)(\d+)", html).group(3)
-    return  int(page_number)
+def get_html(url):
+    f = urllib.request.urlopen(url)
+    return f.read().decode('utf-8')
 
-def __parse_page_html(url, page_number):
-    url += '/page' + str(page_number)
-    return __parse_url_html(url)
 
-def __parse_page_cars(html):
+def __parse_all_pages_url(html):
+    urls = re.findall('(https:\/\/www\.hasznaltauto.hu\/talalatilista\/page.*?)"', html)
+    return urls
+
+def __parse_page_cars(url):
+    html = get_html(url);
     cars = re.findall("contentId.*?(\d+)", html)
     return cars
